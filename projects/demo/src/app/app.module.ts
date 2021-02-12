b85566e7b82eb31d14abf987ea941b3f76d576dd
Bhargav Patel <ibhargav90@gmail.com>
2021-02-12 13:58:09 +0530

    required changes for tabset

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { TabScrollModule } from "ngx-tab-scroll";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 import {
   NgbDropdownModule,
   NgbNavModule,
   NgbTooltipModule,
 } from "@ng-bootstrap/ng-bootstrap";
+
+import { TabScrollModule } from "../../../ngx-tab-scroll/src/lib/tab-scroll.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { TabsetLikeComponent } from "./tabset-like/tabset-like.component";
 
 @NgModule({
